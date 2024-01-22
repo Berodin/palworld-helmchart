7029af5e6ad443504f7024b5f1e13f59e366ba2f
Markus <1994freeway@gmail.com>
2024-01-22 10:35:42 +0100

    initial layout without any testing

---
+++ Dockerfile/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if PUID and PGID are set and not root (0)
+if [[ ! "${PUID}" -eq 0 ]] && [[ ! "${PGID}" -eq 0 ]]; then
+    printf "\e[0;32m*****EXECUTING USERMOD*****\e[0m\n"
+    # Change the user and group ID for the 'steam' user
+    usermod -o -u "${PUID}" steam
+    groupmod -o -g "${PGID}" steam
+else
+    printf "\033[31mRunning as root is not supported, please fix your PUID and PGID!\n"
+    exit 1
+fi
+
+# Create the directory and change its ownership
+mkdir -p /palworld
+chown -R steam:steam /palworld
+
+# If UPDATE_ON_BOOT is set, update the server
+if [ "${UPDATE_ON_BOOT}" = true ]; then
+    printf "\e[0;32m*****STARTING INSTALL/UPDATE*****\e[0m\n"
+    su steam -c '/usr/games/steamcmd +force_install_dir "/palworld" +login anonymous +app_update 2394010 validate +quit'
+fi
+
+# Run the start script
+./start.sh
---
+++ Dockerfile/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Base start command
+STARTCOMMAND="./PalServer.sh"
+
+# Add various options based on environment variables
+[ -n "${PORT}" ] && STARTCOMMAND="${STARTCOMMAND} -port=${PORT}"
+[ -n "${PLAYERS}" ] && STARTCOMMAND="${STARTCOMMAND} -players=${PLAYERS}"
+[ "${COMMUNITY}" = true ] && STARTCOMMAND="${STARTCOMMAND} EpicApp=PalServer"
+[ -n "${PUBLIC_IP}" ] && STARTCOMMAND="${STARTCOMMAND} -publicip=${PUBLIC_IP}"
+[ -n "${PUBLIC_PORT}" ] && STARTCOMMAND="${STARTCOMMAND} -publicport=${PUBLIC_PORT}"
+[ -n "${SERVER_NAME}" ] && STARTCOMMAND="${STARTCOMMAND} -servername=${SERVER_NAME}"
+[ -n "${SERVER_PASSWORD}" ] && STARTCOMMAND="${STARTCOMMAND} -serverpassword=${SERVER_PASSWORD}"
+[ -n "${ADMIN_PASSWORD}" ] && STARTCOMMAND="${STARTCOMMAND} -adminpassword=${ADMIN_PASSWORD}"
+[ "${MULTITHREADING}" = true ] && STARTCOMMAND="${STARTCOMMAND} -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS"
+
+# Change to the game directory
+cd /palworld || exit
+
+# Display the start command (for verification)
+echo "${STARTCOMMAND}"
+
+# Server start message
+printf "\e[0;32m*****STARTING SERVER*****\e[0m\n"
+
+# Execute the start command as 'steam' user
+su steam -c "${STARTCOMMAND}"
